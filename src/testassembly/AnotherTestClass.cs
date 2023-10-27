d5bf94858ba8883a85cb298ed48b3cbb935f96fb
Ted Swenner <snotebooksystem@bk.ru>
2023-10-27 21:58:51 +0300

    Add the assembly to perform unit tests on
    A test assembly is needed for XMLDocParser to work on.

---
+++ src/testassembly/AnotherTestClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Test
+{
+    internal class AnotherTestClass
+    {
+        public AnotherTestClass(Type type) { }
+
+        public event EventHandler<EventArgs> MyEvent;
+
+        public int StructureOfTheWorld = 42;
+
+        public static implicit operator string(AnotherTestClass testClass) => testClass.ToString();
+    }
+}
