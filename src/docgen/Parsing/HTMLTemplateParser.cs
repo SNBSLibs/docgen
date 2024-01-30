5dfcf9be2345e79f86ada1153e18eb6bc38f0992
Ted Swenner <snotebooksystem@bk.ru>
2024-01-30 22:47:46 +0300

    Add the basic structure of HTMLTemplateParser

---
+++ src/docgen/Parsing/HTMLTemplateParser.cs
@@ -0,0 +1,34 @@
+﻿namespace DocGen.Parsing
+{
+    public static class HTMLTemplateParser
+    {
+        public static string Parse(object obj,
+            string? template = null, string? file = null, Stream? stream = null)
+        {
+            ArgumentNullException.ThrowIfNull(obj, nameof(obj));
+            if (template == null && file == null && stream == null)
+                throw new InvalidOperationException("You must specify at least one HTML template source");
+
+            if (template != null) return ParseFromText(template, obj);
+            else if (file != null) return ParseFromFile(file, obj);
+            else if (stream != null) return ParseFromStream(stream, obj);
+            // If docs, file and stream are all null, there will be an exception at line 10
+            // So the following line won't execute
+            else return null!;
+        }
+
+        private static string ParseFromFile(string templatePath, object obj) =>
+            ParseFromFile(File.ReadAllText(templatePath), obj);
+
+        private static string ParseFromStream(Stream stream, object obj)
+        {
+            using var reader = new StreamReader(stream);
+            return ParseFromText(reader.ReadToEnd(), obj);
+        }
+
+        private static string ParseFromText(string template, object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
